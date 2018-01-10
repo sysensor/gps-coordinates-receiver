79e7e8a613ab14094cfb71c5a11b99ff8ade9ff8
Dinuka Malalanayake <dinuka.malalanayake@gmail.com>
2018-01-10 12:43:09 -0600

    Introduce GPS Signal API with GET, POST methods

---
+++ src/main/java/com/sysensor/app/controller/GPSSignalAPI.java
@@ -0,0 +1,33 @@
+package com.sysensor.app.controller;
+
+import com.sysensor.app.config.APIConfig;
+import com.sysensor.app.model.Location;
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(APIConfig.API)
+public class GPSSignalAPI {
+
+    public static final String APPLICATION_JSON = "application/json";
+
+    Logger LOG = Logger.getLogger(this.getClass());
+
+    @RequestMapping(value = "/signal", method = RequestMethod.GET, produces = APPLICATION_JSON)
+    public Location getLatestSignal() {
+        Location location = new Location();
+        location.setLat("41.619549");
+        location.setLng("-93.598022");
+
+        LOG.info("Returning the Latest Location" + location.toString());
+        return location;
+    }
+
+    @RequestMapping(value = "/signal", method = RequestMethod.POST, consumes = APPLICATION_JSON, produces = APPLICATION_JSON)
+    public Location postSignal(Location location) {
+        LOG.info(location.toString());
+        return location;
+    }
+}
---
+++ src/main/java/com/sysensor/app/model/Location.java
@@ -0,0 +1,30 @@
+package com.sysensor.app.model;
+
+public class Location {
+    public String lat;
+    public String lng;
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "lat='" + lat + '\'' +
+                ", lng='" + lng + '\'' +
+                '}';
+    }
+}
